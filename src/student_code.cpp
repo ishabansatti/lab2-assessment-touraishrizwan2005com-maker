e8c792795551e2410ac2b61deed9cfe8fb334bde
touraishrizwan2005com-maker <touraishrizwan2005.com@gmail.com>
2026-02-18 05:18:22 -0800

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -36,7 +36,7 @@ Formula:
 */
 float fahrenheitToCelsius(float fahrenheit) {
     float celsius;
-    celsius = (fahrenheit - 32) * 5/9
+    celsius = (fahrenheit - 32) * 5/9;
     
     //return celsius;
     return celsius;
